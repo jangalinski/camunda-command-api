d8d74e880b24a189d20d9dcefd72d3ec19ec8ca9
Jan Galinski <jan.galinski@holisticon.de>
2020-05-30 21:19:42 +0200

    get annotationprocessortoolkit up and running, wip 3

--- extension/api/value/src/main/java/io/holunda/command/api/value/_CamundaValueTypeGenerator.java
+++ extension/api/value/src/main/java/io/holunda/command/api/value/_CamundaValueTypeGenerator.java
@@ -3,6 +3,6 @@ package io.holunda.command.api.value;
 import io.holunda.command.project.generator.annotation.CamundaValueTypeGenerator;
 
 @CamundaValueTypeGenerator
-interface _CamundaValueTypeGenerator {
+public interface _CamundaValueTypeGenerator {
 
 }
